381aa3e996d582d4e49ff8fdf864761cf97c6811
unknown <aaguilar.g@hotmail.com>
2020-06-23 22:05:51 -0500

    Proyecto actualizado 23062020

--- AppGitNotas/AppGitNotas/MainPage.xaml.cs
+++ AppGitNotas/AppGitNotas/MainPage.xaml.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-
+using System.IO; //Se agrega
 namespace AppGitNotas
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
@@ -13,9 +13,49 @@ namespace AppGitNotas
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        string archivo = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notas.txt");
         public MainPage()
         {
             InitializeComponent();
+
+            if(File.Exists(archivo))
+            {
+                txteditor.Text = File.ReadAllText(archivo);
+            }
+        }
+
+        private void btnGrabar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+            
+            File.WriteAllText(archivo, txteditor.Text);
+            DisplayAlert("Exito", "Archivo grabado con éxito", "OK");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+
+        private void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+            if(File.Exists(archivo))
+            {
+                try 
+                { 
+
+                File.Delete(archivo);
+                txteditor.Text = string.Empty;
+                DisplayAlert("Exito", "Archivo eliminado con éxito", "OK");
+                }
+                catch (Exception ex)
+                {
+                    DisplayAlert("Error", ex.Message, "OK");
+                }
+            }
+            
         }
     }
 }
